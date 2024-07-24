98bc92e953d5e9d6afae7654037cabe33e3c1253
ginn1111 <vanthuanjw@gmail.com>
2024-07-24 18:45:40 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+import * as THREE from "three";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const container = document.getElementById("scene-container");
+  document.write("Hello threejs");
+});
